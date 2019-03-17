3b5f9539f0f71bb31156108b021235b0d8000963
liufeng <15332622786@163.com>
2019-03-17 17:33:14 +0800

    add annotation

--- src/main/java/com/mmall/concurrency/annotation/NotRecommend.java
+++ src/main/java/com/mmall/concurrency/annotation/NotRecommend.java
@@ -6,11 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 用来标记【线程安全】的类或写法
+ * 用来标记不推荐的类或写法
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
-public @interface ThreadSafe {
-
+public @interface NotRecommend {
     String value() default "";
 }
--- src/main/java/com/mmall/concurrency/annotation/Recommend.java
+++ src/main/java/com/mmall/concurrency/annotation/Recommend.java
@@ -6,11 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 用来标记【线程安全】的类或写法
+ * 用来标记推荐的类或写法
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
-public @interface ThreadSafe {
-
+public @interface Recommend {
     String value() default "";
 }
